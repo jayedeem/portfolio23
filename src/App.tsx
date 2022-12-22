af843eb5441d47d157311bd792bc83cb04f7cc1c
Drew C <jayedeem@gmail.com>
2022-12-22 10:32:31 -0800

    forgot i made components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Container, NavBar } from '@/components/'
+import { Container, NavBar, Text, Heading } from '@/components/'
 import styles from './app.module.scss'
 
 function App() {
@@ -13,12 +13,14 @@ function App() {
 				<div>
 					<Container>
 						<div className={styles.heroContainer}>
-							<h4>Hey! I am</h4>
-							<h3 style={{ color: '#fff' }}>Andrew Cacayuran</h3>
-							<h2 style={{ color: '#E0457B' }}>
+							<Heading as="h1">Hey! I am</Heading>
+							<Heading as="h3" style={{ color: '#fff' }}>
+                Andrew Cacayuran
+							</Heading>
+							<Heading as="h3" style={{ color: '#E0457B' }}>
                 I build cool stuff on the web.
-							</h2>
-							<p>
+							</Heading>
+							<Text as="p">
                 I am a software engineer that specializes in full stack
                 development,
 								<br />I currently work at{' '}
@@ -30,13 +32,13 @@ function App() {
 								>
                   Mint Mobile.
 								</a>
-							</p>
+							</Text>
 						</div>
 					</Container>
 					<Container bgColor="#5461C8">
 						<div className={styles.aboutMe}>
-							<h4>About Me</h4>
-							<p style={{ color: '#fff' }}>
+							<Heading as="h4">About Me</Heading>
+							<Text as="p" style={{ color: '#fff' }}>
                 I am Andrew Cacayuran, I have been an avid person in tech. I
                 started off by taking apart my first computer by the age of 15.
                 I am a self taught developer. During covid-19, I learned how to
@@ -44,8 +46,8 @@ function App() {
                 sparked my interest in programing. After a year of teaching
                 myself, I was able to get into the Software Engineering position
                 .
-							</p>
-							<span>Technology I currently work with: </span>
+							</Text>
+							<Text as="p">Technology I currently work with: </Text>
 							<div className={styles.aboutMe__tech}>
 								<ul>
 									<li>Javascript</li>
